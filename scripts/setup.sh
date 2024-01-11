904542df8072c6daa7dcd371ad308959b75f890f
Jan Pfiffner <jan@pfiffner.dev>
2024-01-12 00:00:53 +0100

    add basic ci

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,4 @@
 sudo apt update -y && sudo apt install cmake -y
 sudo apt install gcc-10 gcc-10-base gcc-10-doc g++-10 -y
 sudo apt install libstdc++-10-dev libstdc++-10-doc -y
+sudo apt install lcov libcapture-tiny-perl libdatetime-perl
